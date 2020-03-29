41713e966e934a3ff7995b64e0529b697d1183c3
Jeff Talbot <jeff.a.talbot@gmail.com>
2020-03-29 10:29:08 -0500

    Added words file and seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+File.foreach(Rails.root.join("db", "words.txt")) do |word|
+  Word.create!(value: word)
+end
